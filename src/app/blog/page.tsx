5de6396e8dcf387cf7652bbeb1e8cf220115d969
tuntun <760523867@qq.com>
2024-08-28 23:12:34 +0800

    feat: suspense

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { BlogList } from '@/components/blog-list'
 import { BlogPagination } from '@/components/blog-pagination'
 import { sortBlogs } from '@/lib/utils'
@@ -9,8 +11,12 @@ export default function Blog() {
   return (
     <div className="mx-auto max-w-xl px-8 py-8 md:px-0">
       <h1 className="mb-8 text-center text-2xl font-black">My Blogs 🗒</h1>
-      <BlogList blogs={blogs} />
-      <BlogPagination blogs={blogs} />
+      <Suspense>
+        <BlogList blogs={blogs} />
+      </Suspense>
+      <Suspense>
+        <BlogPagination blogs={blogs} />
+      </Suspense>
     </div>
   )
 }
