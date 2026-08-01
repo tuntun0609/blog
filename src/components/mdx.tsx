abe0b0164df93d67fe6fdac6673536bbbc80ccdd
tuntun <760523867@qq.com>
2026-08-01 16:41:50 +0800

    feat: 添加视频转码工具文档及相关图片，优化博客布局和组件

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,9 +1,11 @@
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
 
 export function getMdxComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: ImageZoom as unknown as NonNullable<MDXComponents['img']>,
     ...components,
   } satisfies MDXComponents
 }
