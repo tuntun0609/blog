d978937ba8a55fd72ae95707658e2df1be735687
tuntun <760523867@qq.com>
2026-08-05 13:01:43 +0800

    feat: 添加运行时启用状态，优化 AvatarSticker 组件的动态显示

--- src/components/home/avatar-sticker-client.tsx
+++ src/components/home/avatar-sticker-client.tsx
@@ -1,7 +1,19 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { AvatarSticker } from './avatar-sticker'
+import { subscribeToHeroTitleReveal } from './hero-animation-sequence'
 
 export function AvatarStickerClient() {
-  return <AvatarSticker isRuntimeEnabled />
+  const [isRuntimeEnabled, setIsRuntimeEnabled] = useState(false)
+
+  useEffect(
+    () =>
+      subscribeToHeroTitleReveal(() => {
+        setIsRuntimeEnabled(true)
+      }),
+    []
+  )
+
+  return <AvatarSticker isRuntimeEnabled={isRuntimeEnabled} />
 }
